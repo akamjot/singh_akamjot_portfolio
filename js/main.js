89d3f9be01ef194bf5413020aea39165e2b45e01
akamjot <sainiparam947@gmail.com>
2024-10-20 11:48:23 -0400

    video controls added and functionality added

---
+++ js/main.js
@@ -0,0 +1,81 @@
+(() => {
+
+
+
+    // Select elements here
+    const playerCon = document.querySelector('#player-container');
+    const player = document.querySelector('video');
+    const videoControls = document.querySelector('#video-controls');
+    const playButton = document.querySelector('#play-button');
+    const pauseButton = document.querySelector('#pause-button');
+    const stopButton = document.querySelector('#stop-button');
+    const volumeSlider = document.querySelector("#change-vol");
+    const fullScreen = document.querySelector("#full-screen");
+    
+    // if JS is loaded then, remove default video controls and show custom controls
+    player.controls = false;
+    videoControls.classList.remove('hidden');
+    
+    
+    // Basic Video controls, similar to what we have done with audio
+    function playVideo() {
+        player.play();
+    }
+    
+    function pauseVideo() {
+        player.pause();
+    }
+    
+    function stopVideo() {
+        player.pause();
+        player.currentTime = 1;
+    }
+    
+    function changeVolume() {
+        player.volume = volumeSlider.value;
+    }
+    
+    // toggleFullScreen toggles the full screen state of the video
+    // If the browser is currently in fullscreen mode,
+    // then it should exit and vice versa.
+    function toggleFullScreen() {
+        if (document.fullscreenElement) {
+          document.exitFullscreen();
+        } else if (document.webkitFullscreenElement) {
+          // Need this to support Safari
+          document.webkitExitFullscreen();
+        } else if (playerCon.webkitRequestFullscreen) {
+          // Need this to support Safari
+            playerCon.webkitRequestFullscreen();
+        } else {
+            playerCon.requestFullscreen();
+        }
+    }
+    
+    // hideControls hides the video controls when not in use
+    // if the video is paused, the controls must remain visible
+    function hideControls() {
+        if (player.paused) {
+          return;
+        } 
+        videoControls.classList.add('hide');
+      }
+      
+      // showControls displays the video controls
+      function showControls() {
+        videoControls.classList.remove('hide');
+      }
+    
+    playButton.addEventListener("click", playVideo);
+    pauseButton.addEventListener("click", pauseVideo);
+    stopButton.addEventListener("click", stopVideo);
+    volumeSlider.addEventListener("change", changeVolume);
+    
+    fullScreen.addEventListener("click", toggleFullScreen);
+    videoControls.addEventListener('mouseenter', showControls);
+    videoControls.addEventListener('mouseleave', hideControls);
+    player.addEventListener('mouseenter', showControls);
+    player.addEventListener('mouseleave', hideControls);
+    
+    
+    })();

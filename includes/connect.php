bcf55bd3e24ed9d0e9361e3ac4ead4138b98cce1
akamjot <sainiparam947@gmail.com>
2025-03-12 19:32:08 -0400

    some changes made in connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -1,9 +1,9 @@
 <?php
 //Creates a connection to the database. This code is 'included' into another file, as if it is pasted into the other file.
 
-$dsn = "mysql:host=localhost;dbname=uwkvfc44_portfolio;charset=utf8mb4";
+$dsn = "mysql:host=localhost;dbname=portfolio;charset=utf8mb4";
 try {
-$connect = new PDO($dsn, 'uwkvfc44_akamjot', '@akamjot12');
+$connect = new PDO($dsn, 'root', '');
 } catch (Exception $e) {
   error_log($e->getMessage());
   exit('unable to connect');
